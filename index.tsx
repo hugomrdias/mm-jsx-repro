8d5b484aa2180f86881e03da353a531bac4465b1
Hugo Dias <hugomrdias@gmail.com>
2024-09-05 15:20:43 +0100

    first commit

---
+++ index.tsx
@@ -0,0 +1,10 @@
+import { Box, Heading, Text } from '@metamask/snaps-sdk/jsx'
+
+export function Test() {
+  return (
+    <Box>
+      <Heading>Alert heading</Heading>
+      <Text>Something happened in the system.</Text>
+    </Box>
+  )
+}
